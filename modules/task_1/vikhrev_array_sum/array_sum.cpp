4f33496817d059d20195a7ec64ace4421663f0e0
ivanvikhrev <laind4471@gmail.com>
2018-10-11 21:40:28 +0300

    update

--- modules/task_1/vikhrev_array_sum/array_sum.cpp
+++ modules/task_1/vikhrev_array_sum/array_sum.cpp
@@ -2,26 +2,35 @@
 #include <iostream>
 #include <assert.h>
 #include <cstdlib>
+#include <time.h>
+
 #define MainProc 0
+#define OutputSize 26
+
 int main(int argc, char* argv[])
 {   
 	// mpi variables
-	int status, rank, size;
+	int status, rank, size, nProc = 0;
 	double t1, t2;
 	//usual variables
-	int  column,
-		rows,
-		arrSize,
-		partialSum = 0,
-		arrSum = 0,
-		nProc = 0,
-		N; // number of elem that will be given to one process
-	int *arr, //matrix
-		*buff; // buffer for message exchanging
-	//init
-	rows = (atoi(argv[1]) > 0) ? atoi(argv[1]) : 10;
-	column = (atoi(argv[2]) > 0) ? atoi(argv[2]) : 10;
-	arrSize = rows * column;
+	int  cols,
+		 rows,
+		 arrSize,
+		 N; // number of elems that will be given to one process
+	double *arr, //matrix
+		   *buff, // buffer for messages
+		    partialSum = 0,
+		    arrSum = 0;
+	
+	//read argv
+	if (argc > 2)
+	{
+		rows = atoi(argv[1]);
+		cols =  atoi(argv[2]);
+	}
+	else
+		rows = cols = 10;
+	arrSize = rows * cols;
 
 	//mpi part
 	status = MPI_Init(&argc, &argv);
@@ -34,25 +43,45 @@ int main(int argc, char* argv[])
 	assert(status == MPI_SUCCESS);
 
 	if (size > 64) return -1; // limit 
-	if (rank == MainProc) t1 = MPI_Wtime();
 	
 	nProc = (arrSize >= size) ? size : arrSize;
 	N = arrSize / nProc;
 	
 	if (rank == MainProc) // sending
-	{
-		arr = new int[arrSize];
-
+	{	
+		//init arr
+		arr = new double[arrSize];
+		srand((unsigned)time(NULL));
 		for (int i = 0; i < arrSize; i++)
-			arr[i] = 1;
-
+			arr[i] = (rand()%100)/100.0;
+		
+		if (arrSize < OutputSize)
+		{
+			for (int i = 0; i < arrSize; i++)
+			{
+				std::cout << arr[i] << " ";
+				if((i+1) % cols == 0)
+					std::cout << std::endl;
+			}
+		}
+		std::cout << std::endl;
 		buff = arr;
-
+	
+		//sequential part
+		t1 = MPI_Wtime(); 
+		for (int i = 0; i < arrSize; i++)
+			arrSum += arr[i];
+		t2 = MPI_Wtime();
+		std::cout << "Sequential Time: " << t2 - t1 << std::endl;
+		std::cout << "Sequential Sum = " << arrSum << std::endl;
+		//end sequential part
+		
+		t1 = MPI_Wtime(); // start 
 		for (int i = 1; i < nProc - 1; i++)
-			status = MPI_Send(&arr[i*N], N, MPI_INT, i, i, MPI_COMM_WORLD);
+			status = MPI_Send(&arr[i*N], N, MPI_DOUBLE, i, i, MPI_COMM_WORLD);
 
 		if (size != 1) // sending last part separately in case if arrSize % size != 0
-		    MPI_Send(&arr[N*(nProc - 1)], arrSize - N * (nProc - 1), MPI_INT, nProc - 1, nProc - 1, MPI_COMM_WORLD);
+		    MPI_Send(&arr[N*(nProc - 1)], arrSize - N * (nProc - 1), MPI_DOUBLE, nProc - 1, nProc - 1, MPI_COMM_WORLD);
 	
 
 	}
@@ -61,49 +90,47 @@ int main(int argc, char* argv[])
 		if (rank == nProc - 1)
 		{
 			N = arrSize - N * (nProc - 1);
-			buff = new int[N];
-		    MPI_Recv(buff, N, MPI_INT, MainProc, rank, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
+			buff = new double[N];
+		    MPI_Recv(buff, N, MPI_DOUBLE, MainProc, rank, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
 		}
 		else
 		{
-			buff = new int[N];
-			MPI_Recv(buff, N, MPI_INT, MainProc, rank, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
+			buff = new double[N];
+			MPI_Recv(buff, N, MPI_DOUBLE, MainProc, rank, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
 		}
 	}
 
 	//common part 
 	for (int i = 0; i < N; i++)
 		partialSum += buff[i];
-
+	
+	//sum
 	if (rank == MainProc)
-	{
+	{	
+		arrSum = 0;
 		arrSum += partialSum;
 		
 		for (int i = 1; i < size; i++)
 		{
-			MPI_Recv(&partialSum, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
+			MPI_Recv(&partialSum, 1, MPI_DOUBLE, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
 			arrSum += partialSum;
 		}
-
-		std::cout << arrSum << std::endl;
+		t2 = MPI_Wtime();
+		std::cout << std::endl;
+		std::cout << "Parallel Time: " << t2 - t1 << std::endl;
+		std::cout <<"Parallel Sum = " << arrSum << std::endl;
 	}
 	else
 	{
-		MPI_Send(&partialSum, 1, MPI_INT, MainProc, 0, MPI_COMM_WORLD);
+		MPI_Send(&partialSum, 1, MPI_DOUBLE, MainProc, 0, MPI_COMM_WORLD);
 	}
-
-
-
+	
+	//del
 	if (rank == 0)
 		delete[] arr;
 	else
 		delete[] buff;
-		
-	if (rank == MainProc)
-	{
-		t2 = MPI_Wtime();
-		std::cout << "Wtime: " << t2 - t1 << std::endl;
-	}
+
 	status = MPI_Finalize();
 	assert(status == MPI_SUCCESS);
 	return 0;

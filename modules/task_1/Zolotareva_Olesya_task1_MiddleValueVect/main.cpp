e835a3786fbc910d7209b7b619b93c273f8b43dd
lesya89 <zolota98@mail.ru>
2018-11-02 09:45:11 +0300

    modif main

--- modules/task_1/Zolotareva_Olesya_task1_MiddleValueVect/main.cpp
+++ modules/task_1/Zolotareva_Olesya_task1_MiddleValueVect/main.cpp
@@ -6,6 +6,7 @@
 #include <cstdlib>
 #include <assert.h>
 
+using namespace std;
 int main(int argc, char *argv[])
 {
 
@@ -20,82 +21,118 @@ int main(int argc, char *argv[])
     else
         N = 50;
 
-    x = new int[N];
+     
 
-    for(int i=0;i<N;i++) x[i] = rand()%20 + 1;
-
-
-    int ProcRank, ProcNum;
+    int ProcRank, ProcNum,flag;
 
     MPI_Status Status;
 
+	x = new int[N];
+
+	for (int i = 0; i < N; i++) x[i] = rand() % 2000 + 1;
+	if (N < 50)
+	{
+		cout << "x:  ";
+		for (int i = 0; i < N; i++) cout << x[i] << " ";
+	}
+
     MPI_Init(&argc,&argv);
+	MPI_Initialized(&flag);
+
+	if (!flag)
+	{
+		cout << "Error";
+		delete[] x;
+		return 0;
+	}
+
     MPI_Comm_size(MPI_COMM_WORLD,&ProcNum);
     MPI_Comm_rank(MPI_COMM_WORLD,&ProcRank);
-    int ParallelSum = 0;
-    double ParallelTimeStart=0;
-    int tempSum = 0;
-    if ( ProcRank == 0 )
-    {
-        //serial v
+		int ParallelSum = 0;
+	//double tempsum = 0;
+		double ParallelTimeStart=0;
+		double ParallelTimeEnd = 0;
+		int tempSum = 0;
+    
+	if (ProcRank == 0)
+	{
+		//serial v
 
-        int SerialSum = 0;
-        double SerialTimeStart = MPI_Wtime();
+		int SerialSum = 0;
+		double SerialTimeStart = MPI_Wtime();
 
-        for(int i=0;i<N;i++)
-        {
-            SerialSum+=x[i];
-        }
-        double SerialMidscore = (double)SerialSum/N;
-        double SerialTime = MPI_Wtime() - SerialTimeStart;
+		for (int i = 0; i < N; i++)
+		{
+			SerialSum += x[i];
+		}
+		double SerialMidscore = (double)SerialSum / N;
+		double SerialTimeEnd = MPI_Wtime();
 
-        printf("\nMidscore (serial) = %3.3f",SerialMidscore);
-        printf("\nTime (serial) = %3.6f",SerialTime);
+		//double SerialTime = MPI_Wtime() - SerialTimeStart;
 
-        //parallel v
+		cout << "Midscore (line) = " << SerialMidscore<<endl; 
+		cout << "Time (line) = " << SerialTimeEnd - SerialTimeStart<<endl;
 
-        ParallelTimeStart = MPI_Wtime();
-        int p = N/ProcNum;
-        for(int i=1;i<ProcNum-1;i++)
-        {
-            int jn = p*i;
-            int jk = p*(i+1);
-            if(i==ProcNum - 1) jk = N;
-            int blockSize = jk - jn;
-            MPI_Send(&blockSize, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-            MPI_Send(x+jn, blockSize, MPI_INT, i, 0, MPI_COMM_WORLD);
-        }
-        int blockSize = N - p*(ProcNum - 1);
-        MPI_Send(&blockSize, 1, MPI_INT, ProcNum-1, 0, MPI_COMM_WORLD);
-        MPI_Send(x+p*(ProcNum - 1), blockSize, MPI_INT, ProcNum-1, 0, MPI_COMM_WORLD);
-        for(int i=0; i<N/ProcNum;i++)
-        {
-            ParallelSum+=x[i];
-        }
-    }
+		//parallel v
+		if (ProcNum > 1)
+		{
+			ParallelTimeStart = MPI_Wtime();
+			int p = N / ProcNum ;
+			for (int i = 1; i < ProcNum - 1; i++)
+			{
+
+				// MPI_Send(&blockSize, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+				MPI_Send((x + i * p), p, MPI_INT, i, 0, MPI_COMM_WORLD);
+			}
+
+			// int blockSize = N - p*(ProcNum - 1);
+			 //MPI_Send(&blockSize, 1, MPI_INT, ProcNum-1, 0, MPI_COMM_WORLD);
+			MPI_Send((x + (ProcNum - 1)*p),( N- (ProcNum - 1)*p), MPI_INT, (ProcNum - 1), 0, MPI_COMM_WORLD);
+			for (int i = 0; i < p; i++)
+			{
+				ParallelSum += x[i];
+			}
+
+			for (int i = 1; i < ProcNum; i++)
+			
+			{
+				MPI_Recv(&tempSum, 1, MPI_INT, i , 0, MPI_COMM_WORLD, &Status);
+				ParallelSum += tempSum;
+			}
+
+			double ParallelTimeEnd = MPI_Wtime();
+			cout << "\nMidScore(parallel)=" << (double)ParallelSum / N<<endl;
+			cout << "\n Time(parallel)=" << (ParallelTimeEnd - ParallelTimeStart)<<endl;
+		}
+	}
     else
-    {
-        int blockSize;
-        int *temp;
-        MPI_Recv(&blockSize, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &Status);
-        temp = new int[blockSize];
-        MPI_Recv(temp, blockSize, MPI_INT, 0, 0, MPI_COMM_WORLD, &Status);
-        for(int i=0;i<blockSize;i++)
-        {
-            tempSum+=temp[i];
-        }
-        delete []temp;
-    }
-    int sum = 0;
-    MPI_Reduce(&tempSum, &sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-    ParallelSum+=sum;
-    if(ProcRank==0)
-    {
-        double ParallelMidscore = (double)ParallelSum/N;
-        double ParallelTime = MPI_Wtime() - ParallelTimeStart;
-        printf("\nMidscore (parallel) = %3.3f",ParallelMidscore);
-        printf("\nTime (parallel) = %3.6f",ParallelTime);
-    }
+	{
+		if (ProcRank != ProcNum-1) {
+
+
+			int blockSize = N / ProcNum ;
+			int *temp;
+			temp = new int[blockSize];
+			//MPI_Recv(&blockSize, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &Status);
+			MPI_Recv(temp, blockSize, MPI_INT, 0, 0, MPI_COMM_WORLD, &Status);
+			for (int i = 0; i < blockSize; i++)
+			{
+				tempSum += temp[i];
+			}
+			MPI_Send(&tempSum, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+			//delete []temp;
+		}
+		else {
+		int blockSize = N - (ProcNum - 1)*(N / ProcNum);
+		int *temp;
+		temp = new int[blockSize];
+		MPI_Recv(temp, blockSize, MPI_INT, 0, 0, MPI_COMM_WORLD, &Status);
+		for (int i = 0; i < blockSize; i++)
+			tempSum += temp[i];
+		MPI_Send(&tempSum,1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+	}}
     MPI_Finalize();
+
     delete []x;
+	
 }

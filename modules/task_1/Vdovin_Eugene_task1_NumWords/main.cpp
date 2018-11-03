011e773b4b7881057a8b09f9a1c0a05f219e2156
J-win <j.vdowin@yandex.ru>
2018-11-03 13:06:31 +0300

    add send and recv

--- modules/task_1/Vdovin_Eugene_task1_NumWords/main.cpp
+++ modules/task_1/Vdovin_Eugene_task1_NumWords/main.cpp
@@ -9,49 +9,51 @@ using namespace std;
 
 int converter_in_number(const string &s)
 {
-    int len = s.length();
-    int a = 0;
-    int i = 0;
-    for (i = 0; (i < len); i++)
-        a = a * 10 + (s[i] - '0');
-    return a;
+	int len = s.length();
+	int a = 0;
+	int i = 0;
+	for (i = 0; (i < len); i++)
+		a = a * 10 + (s[i] - '0');
+	return a;
 }
 
 int main(int argc, char *argv[])
 {
-    srand((int)time(0));
+	srand((int)time(0));
 
-    string size = argv[1];
-    string l = "qwertyuiop asdfghjkl zxcvbnm";
-    int lenl = l.length();
-    int len = converter_in_number(size);
-    len = len * 3;
-	char* s = new char[len + 1];
-	int k = 0;
-	while (k < len - 2)
-	{
-		s[k] = 'a';
-		s[k + 1] = l[(int)(rand() % lenl)];
-		s[k + 2] = 'a';
-		k = k + 3;
-	}
+	string size = argv[1];
+	string l = "qwertyuiop asdfghjkl zxcvbnm";
+	int lenl = l.length();
+	int len = converter_in_number(size);
+	len = len * 3;
+	char* s;
 
-    int ProcNum, ProcRank;
-    double stime = 0.0;
-    double etime = 0.0;
-    double stime_ = 0.0;
-    double etime_ = 0.0;
-    int nword = 0;
-    int nword_ = 0;
-    int nresword = 0;
-    int residue = 0;
+	int ProcNum, ProcRank;
+	double stime = 0.0;
+	double etime = 0.0;
+	double stime_ = 0.0;
+	double etime_ = 0.0;
+	int nword = 0;
+	int nword_ = 0;
+	int nresword = 0;
+	int residue = 0;
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
-    MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+	MPI_Init(&argc, &argv);
+	MPI_Status ss;
+	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
+	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
 
 	if (ProcRank == 0)
 	{
+		s = new char[len + 1];
+		int k = 0;
+		while (k < len - 2)
+		{
+			s[k] = 'a';
+			s[k + 1] = l[(int)(rand() % lenl)];
+			s[k + 2] = 'a';
+			k = k + 3;
+		}
 		stime = MPI_Wtime();
 		for (int i = 0; i < len; i++)
 			if (s[i] == ' ')
@@ -62,29 +64,36 @@ int main(int argc, char *argv[])
 		cout << "Number words: " << nword << endl;
 		cout << "Time: " << etime - stime << " sec" << endl;
 		stime_ = MPI_Wtime();
+		for (int i = 1; i < ProcNum; i++)
+			MPI_Send(s + len / ProcNum * i, len / ProcNum, MPI_CHAR, i, 0, MPI_COMM_WORLD);
+	}
+	else
+	{
+		s = new char[len / ProcNum + 1];
+		MPI_Recv(s, len / ProcNum, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &ss);
 	}
 
-	for (int i = len / ProcNum * ProcRank; i < len / ProcNum + len / ProcNum * ProcRank; i++)
+	for (int i = 0; i < len / ProcNum; i++)
 		if (s[i] == ' ')
 			nword_++;
 
-    MPI_Reduce(&nword_, &nresword, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+	MPI_Reduce(&nword_, &nresword, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
-    if (ProcRank == 0)
-    {
-        residue = len % ProcNum;
-        if (residue)
-            for (int i = len - residue; i < len; i++)
-                if (s[i] == ' ')
-                    nresword++;
-        nresword++;
-        etime_ = MPI_Wtime();
-        cout << ProcNum << " process" << endl;
-        cout << "Number words: " << nresword << endl;
-        cout << "Time: " << etime_ - stime_ << " sec" << endl;
-    }
+	if (ProcRank == 0)
+	{
+		residue = len % ProcNum;
+		if (residue)
+			for (int i = len - residue; i < len; i++)
+				if (s[i] == ' ')
+					nresword++;
+		nresword++;
+		etime_ = MPI_Wtime();
+		cout << ProcNum << " process" << endl;
+		cout << "Number words: " << nresword << endl;
+		cout << "Time: " << etime_ - stime_ << " sec" << endl;
+	}
 
-    MPI_Finalize();
-    delete[] s;
-    return 0;
+	MPI_Finalize();
+	delete[] s;
+	return 0;
 }

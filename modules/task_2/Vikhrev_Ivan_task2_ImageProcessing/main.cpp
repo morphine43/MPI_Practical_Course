fac347ab454f1d0e65d1552b315bd70944b458ac
ivanvikhrev <laind4471@gmail.com>
2018-11-15 21:16:02 +0300

    fix code style2

--- modules/task_2/Vikhrev_Ivan_task2_ImageProcessing/main.cpp
+++ modules/task_2/Vikhrev_Ivan_task2_ImageProcessing/main.cpp
@@ -3,41 +3,39 @@
 #include <assert.h>
 #include <mpi.h>
 #include <iostream>
+
 #define MainProc 0
 #define ksize 21
-using namespace cv;
 
-class img
-{
+class img {
     public:
-    String imageName;
-    Mat pic;
+    cv::String imageName;
+    cv::Mat pic;
     unsigned int cols;
     unsigned int rows;
     unsigned int  type;
     unsigned int  imgSize;
     img();
-    img(Mat m);
+    img(cv::Mat m);
     img(const img &m);
-    ~img();
-    bool loadImg(String name);
-    bool saveImg(String name);
+	~img() {
+	};
+    bool loadImg(cv::String name);
+    bool saveImg(cv::String name);
     void createImg(unsigned int height, unsigned int width);
-    void show(String wName);
+    void show(cv::String wName);
 };
 
-img::img()
-{
+img::img() {
   imageName = "";
-  pic = Mat();
+  pic = cv::Mat();
   cols = 0;
   rows = 0;
   type = 0;
   imgSize = 0;
 }
 
-img::img(const img& m)
-{
+img::img(const img& m) {
   imageName = m.imageName;
   m.pic.copyTo(pic);
   cols = m.cols;
@@ -46,8 +44,7 @@ img::img(const img& m)
   imgSize = cols * rows;
 }
 
-img::img(Mat m)
-{  
+img::img(cv::Mat m) {  
   imageName = "";
   m.copyTo(pic);
   cols = m.cols;
@@ -56,12 +53,8 @@ img::img(Mat m)
   imgSize = cols * rows;
 }
 
-img::~img()
-{}
-
-bool img::loadImg(String name)
-{
-  String imageName = name;
+bool img::loadImg(cv::String name) {
+  cv::String imageName = name;
   pic = imread(imageName, 0);
   if (pic.empty()) return false;
   cols = pic.cols;
@@ -71,39 +64,32 @@ bool img::loadImg(String name)
   return true;
 }
 
-bool img::saveImg(String name)
-{
-  return imwrite(name, pic);
+bool img::saveImg(cv::String name) {
+  return cv::imwrite(name, pic);
 }
 
-void img::createImg(unsigned int height, unsigned int width)
-{
-  RNG r;
+void img::createImg(unsigned int height, unsigned int width) {
+  cv::RNG r;
   cols = width;
   rows = height;
   imgSize = cols*rows;
   type = CV_8U;
-  pic = Mat(rows, cols, CV_8U, cv::Scalar(0));
-  r.fill(pic, RNG::UNIFORM, cv::Scalar(0), cv::Scalar(255));
+  pic = cv::Mat(rows, cols, CV_8U, cv::Scalar(0));
+  r.fill(pic, cv::RNG::UNIFORM, cv::Scalar(0), cv::Scalar(255));
 }
 
-void applyFilter1(Mat& img, int kernel_size)
-{
-  medianBlur(img, img, kernel_size);
+void applyFilter1(cv::Mat& img, int kernel_size) {
+  cv::medianBlur(img, img, kernel_size);
 }
-void applyFilter2(Mat& img, int kernel_size)
-{
-  blur(img, img, Size(kernel_size, kernel_size));
+void applyFilter2(cv::Mat& img, int kernel_size) {
+  cv::blur(img, img, cv::Size(kernel_size, kernel_size));
 }
-void applyFilter3(Mat& img, int kernel_size)
-{
-  GaussianBlur(img, img, Size(kernel_size, kernel_size), 0);
+void applyFilter3(cv::Mat& img, int kernel_size) {
+  cv::GaussianBlur(img, img, cv::Size(kernel_size, kernel_size), 0);
 }
-void img::show(String wName)
-{
-  if (rows <= 1080 && cols <= 1920)
-  {
-    imshow(wName, pic);
+void img::show(cv::String wName) {
+  if (rows <= 1080 && cols <= 1920) {
+    cv::imshow(wName, pic);
   }
 }
 
@@ -120,22 +106,21 @@ const char* kOptions =
 "{ s show        | false  |  show image                }"
 "{ h ? help usage|        |  print help message        }";
 
-int main(int argc, char** argv)
-{
-  CommandLineParser parser(argc, argv, kOptions);
+int main(int argc, char** argv) {
+  cv::CommandLineParser parser(argc, argv, kOptions);
   parser.about(kAbout);
 
   int status, rank, size;
   img origImg, procImgS, procImgP;
   unsigned int cols = 0,
-             rows = 0,
-             image_size = 0,
+               rows = 0,
+               image_size = 0,
                type = 0,
                portion = 0,
                filter = 1;
   int *scount = nullptr,
-    *dis =    nullptr;
-  String imageName;
+      *dis =    nullptr;
+  cv::String imageName;
   uchar  *dataIN =  nullptr,
           *dataOUT = nullptr,
        *p = nullptr;
@@ -143,40 +128,41 @@ int main(int argc, char** argv)
   bool checkImg = true;
 
   status = MPI_Init(&argc, &argv);
-  if (status != MPI_SUCCESS) { return -1; }
+  if (status != MPI_SUCCESS) { 
+    return -1; 
+  }
 
   status = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  if (status != MPI_SUCCESS) { return -1; }
+  if (status != MPI_SUCCESS) { 
+    return -1; 
+  }
+
 
   status = MPI_Comm_size(MPI_COMM_WORLD, &size);
-  if (status != MPI_SUCCESS) { return -1; }
+  if (status != MPI_SUCCESS) {
+    return -1; 
+  }
 
-  if (parser.get<bool>("help"))
-  {
+  if (parser.get<bool>("help")) {
     if (rank == MainProc )
       parser.printMessage();
     MPI_Finalize();
     return 0;
   }
 
-  if (rank == MainProc)
-  {
-    if (parser.has("i"))
-    {
-      imageName = parser.get<String>("i");
+  if (rank == MainProc) {
+    if (parser.has("i")) {
+      imageName = parser.get<cv::String>("i");
       checkImg = origImg.loadImg(imageName);
       std::cout << "Path to image: " << imageName << std::endl;;
-      if (!checkImg)
-      {
+      if (!checkImg) {
         std::cout << "Cant load image" << std::endl;
         MPI_Abort(MPI_COMM_WORLD, 1);
       }
 
     }
-    else
-    {
-      if (parser.get<bool>("r"))
-      {
+    else{
+      if (parser.get<bool>("r")) {
         std::cout << "Random Image" << std::endl;
         rows = parser.get<unsigned int>("y");
         std::cout << "Rows: " << rows << std::endl;
@@ -184,14 +170,11 @@ int main(int argc, char** argv)
         std::cout << "Cols: " << cols << std::endl;
         origImg.createImg(rows, cols);
       }
+	}
 
-    }
-
-    if (parser.has("f"))
-    {
+    if (parser.has("f")) {
       filter = parser.get<unsigned int>("f");
-      if (filter != 1 && filter != 2 && filter != 3)
-      {
+      if (filter != 1 && filter != 2 && filter != 3){
         std::cout << "filter`s value 1,2 or 3"<< std::endl;
         filter = 1;
       }
@@ -230,19 +213,18 @@ int main(int argc, char** argv)
   dis = new int[size];
   scount = new int[size];
   
-  for (int i = 0; i < size; ++i) 
-  {
+  for (int i = 0; i < size; ++i) {
     dis[i] = i*portion -cols * ksize;
     scount[i] = portion + 2*cols * ksize;
-    if (i == size - 1 || i == 0)
-    {
+    if (i == size - 1 || i == 0){
       dis[0] = 0;
       scount[i] = portion + cols * ksize;
     }
   }
-  MPI_Scatterv(origImg.pic.data, scount, dis, MPI_UNSIGNED_CHAR, dataIN, scount[rank], MPI_UNSIGNED_CHAR, MainProc, MPI_COMM_WORLD);
+  MPI_Scatterv(origImg.pic.data, scount, dis, MPI_UNSIGNED_CHAR, dataIN, scount[rank],
+    MPI_UNSIGNED_CHAR, MainProc, MPI_COMM_WORLD);
 
-  Mat tmp(scount[rank] / cols, cols, CV_8U, dataIN);
+  cv::Mat tmp(scount[rank] / cols, cols, CV_8U, dataIN);
   
   if (filter == 1)
     applyFilter1(tmp, ksize);
@@ -254,21 +236,19 @@ int main(int argc, char** argv)
 
   p = tmp.data + cols * ksize;//pointer do data
   if (rank == MainProc) p = tmp.data;
-  MPI_Gather(p, portion, MPI_UNSIGNED_CHAR, dataOUT, portion, MPI_UNSIGNED_CHAR, MainProc, MPI_COMM_WORLD);
+  MPI_Gather(p, portion, MPI_UNSIGNED_CHAR, dataOUT, portion,
+    MPI_UNSIGNED_CHAR, MainProc, MPI_COMM_WORLD);
 
-    if (rank == MainProc)
-  {
+    if (rank == MainProc) {
     t2 = MPI_Wtime();
-    procImgP = img(Mat(origImg.rows, origImg.cols, CV_8U, dataOUT));
+    procImgP = img(cv::Mat(origImg.rows, origImg.cols, CV_8U, dataOUT));
     std::cout << "Parallel Time:   " << t2 - t1 << std::endl;
     
-    if (parser.get<bool>("s"))
-    {  
-  
-      origImg.show((String)"Original");
-      procImgS.show((String)"Sequnce processed");
-      procImgP.show((String)"Parallel processed");
-      waitKey(0);
+    if (parser.get<bool>("s")) {  
+      origImg.show(static_cast<cv::String>("Original"));
+      procImgS.show(static_cast<cv::String>("Sequnce processed"));
+      procImgP.show(static_cast<cv::String>("Parallel processed"));
+      cv::waitKey(0);
     }
     if (!origImg.saveImg("Original.png"))
       std::cout << " Can`t save image 0 " << std::endl;
@@ -276,7 +256,7 @@ int main(int argc, char** argv)
        std::cout << " Can`t save image 1" << std::endl;
     if(!procImgP.saveImg("smooth_image_par.png"))
       std::cout << " Can`t save image 2" << std::endl;
-  }
+    }
   delete[] dataIN;
   delete[] dataOUT;
   delete[] dis;

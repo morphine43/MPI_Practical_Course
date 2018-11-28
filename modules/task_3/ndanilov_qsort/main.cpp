3ab91f598cc7aa9c167559bbfd73eb4e8b9ae6ef
Nikita Danilov <nikita.danilov@aquantia.com>
2018-11-28 17:23:50 +0300

    added CI fixed linter warnigs

--- modules/task_3/ndanilov_qsort/main.cpp
+++ modules/task_3/ndanilov_qsort/main.cpp
@@ -5,6 +5,10 @@
 #include <time.h>
 #include <limits.h>
 #include <memory.h>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <string>
 
 #define N 1000000
 #define DEBUG 0
@@ -13,29 +17,26 @@
 double start_time, stop_time;
 
 #if DEBUG
-void print_info(int id, char *m)
-{
-    printf("%d: %s %f secs\n", id, m, (clock() - start_time) / CLOCKS_PER_SEC);
+void print_info(int id, std::string msg) {
+    std::cout << id << ": " << msg << (clock() - start_time) / CLOCKS_PER_SEC
+              << " secs\n";
 }
 #endif
 
-void print_vector(int *v, int vector_size)
-{
+void print_vector(int *v, int vector_size) {
     int i;
 
     for (i = 0; i < vector_size; i++)
         printf("\tv[%2d] = %d\n", i, v[i]);
-    
 }
 
-int *merge(int *v1, int n1, int *v2, int n2)
-{
+int *merge(int *v1, int n1, int *v2, int n2) {
     int *result;
     int i = 0,
         j = 0,
         k = 0;
 
-    result = (int *)malloc((n1 + n2) * sizeof(int));
+    result = new int[n1 + n2];
 
     while (i < n1 && j < n2) {
         if (v1[i] < v2[j]) {
@@ -62,8 +63,7 @@ int *merge(int *v1, int n1, int *v2, int n2)
     return result;
 }
 
-void swap(int *v, int i, int j)
-{
+void my_swap(int *v, int i, int j) {
     int t;
 
     t = v[i];
@@ -71,26 +71,24 @@ void swap(int *v, int i, int j)
     v[j] = t;
 }
 
-void quick_sort(int *v, int left, int right)
-{
-    int i,last;
+void quick_sort(int *v, int left, int right) {
+    int i, last;
 
     if (left >= right)
         return;
 
-    swap(v, left, (left + right) / 2);
+    my_swap(v, left, (left + right) / 2);
     last = left;
     for (i = left + 1; i <= right; i++)
-        if(v[i] < v[left])
-            swap(v, ++last, i);
+        if (v[i] < v[left])
+            my_swap(v, ++last, i);
 
-    swap(v, left, last);
+    my_swap(v, left, last);
     quick_sort(v, left, last-1);
     quick_sort(v, last + 1, right);
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     double seq_time, par_time;
     int m, vector_size = N;
     int id, proc_number;
@@ -120,18 +118,18 @@ int main(int argc, char **argv)
         int extra_elements;
         int data_size;
 
-        srand(time(NULL) + clock());
+        std::srand(unsigned(std::time(NULL)));
 
         extra_elements = vector_size % proc_number;
         data_size = vector_size + extra_elements;
         chunk_size = vector_size / proc_number;
 
-        /* alloc memory for vector
+        /* allocate memory for vector
          * increase the size of the array so that
          * it is divided without a balance by the number of processors
          */
-        data   = (int *)malloc(data_size * sizeof(int));
-        datacp = (int *)malloc(vector_size * sizeof(int));
+        data   = new int[data_size];
+        datacp = new int[vector_size];
 
         /* fill main elements of vector
          * rand() + rand() can give overflow int,
@@ -140,7 +138,7 @@ int main(int argc, char **argv)
          * rand() returns non-negative numbers
          */
         for (i = 0; i < vector_size; i++)
-            data[i] = rand() + rand();
+            data[i] = std::rand() + std::rand();
 
         /* copy data to another array for sequential sorting */
         memcpy(datacp, data, vector_size * sizeof(int));
@@ -167,7 +165,7 @@ int main(int argc, char **argv)
 #endif
 
         MPI_Bcast(&chunk_size, 1, MPI_INT, MASTER_PROCESS, MPI_COMM_WORLD);
-        chunk = (int *)malloc(chunk_size * sizeof(int));
+        chunk = new int[chunk_size];
         MPI_Scatter(data, chunk_size, MPI_INT, chunk, chunk_size, MPI_INT,
                     MASTER_PROCESS, MPI_COMM_WORLD);
 
@@ -183,7 +181,7 @@ int main(int argc, char **argv)
 
     } else {
         MPI_Bcast(&chunk_size, 1, MPI_INT, MASTER_PROCESS, MPI_COMM_WORLD);
-        chunk = (int *)malloc(chunk_size * sizeof(int));
+        chunk = new int[chunk_size];
         MPI_Scatter(data, chunk_size, MPI_INT, chunk, chunk_size, MPI_INT,
                     MASTER_PROCESS, MPI_COMM_WORLD);
 
@@ -196,7 +194,6 @@ int main(int argc, char **argv)
 #if DEBUG
         print_info(id, "Sorted");
 #endif
-
     }
 
     step = 1;
@@ -206,7 +203,7 @@ int main(int argc, char **argv)
             if (id + step < proc_number) {
                 MPI_Recv(&m, 1, MPI_INT, id + step, MASTER_PROCESS,
                          MPI_COMM_WORLD, &status);
-                other = (int *)malloc(m * sizeof(int));
+                other = new int[m];
                 MPI_Recv(other, m, MPI_INT, id + step, MASTER_PROCESS,
                          MPI_COMM_WORLD, &status);
 
@@ -220,9 +217,9 @@ int main(int argc, char **argv)
                 print_info(id, "Merged data");
 #endif
 
-                free(other);
+                delete[] other;
                 chunk_size += m;
-            } 
+            }
         } else {
             int near = id - step;
             MPI_Send(&chunk_size, 1, MPI_INT, near, MASTER_PROCESS,
@@ -272,10 +269,7 @@ int main(int argc, char **argv)
         }
     }
 
-    free(chunk);
-
-    if (id == MASTER_PROCESS)
-        free(data);
+    delete[] chunk;
 
     MPI_Finalize();
 
@@ -295,7 +289,10 @@ int main(int argc, char **argv)
             print_vector(datacp, vector_size);
         }
 
-        printf("\nAcceleration was: %f%%\n", ((seq_time / par_time) -1 ) * 100);
+        printf("\nAcceleration was: %f\n", (seq_time / par_time));
+
+        delete[] datacp;
+        delete[] data;
     }
     return 0;
 }
